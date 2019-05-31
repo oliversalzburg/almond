d90ab1393e4c18cfd4b2c17b6f56c7737bcae697
Oliver Salzburg <oliver.salzburg@gmail.com>
2019-05-31 21:19:44 +0200

    feat: Rotate image in

--- main.js
+++ main.js
@@ -3,10 +3,10 @@
 const nodeCanvas = document.getElementById( "main" );
 const context    = nodeCanvas.getContext( "2d" );
 
-const width  = nodeCanvas.width;
-const height = nodeCanvas.height;
+const WIDTH  = nodeCanvas.width;
+const HEIGHT = nodeCanvas.height;
 
-const pixMap = context.createImageData( width, height );
+const pixMap = context.createImageData( WIDTH, HEIGHT );
 
 function fractal( x, y, maxIterations, maxRadius ) {
 	let real      = x;
@@ -28,20 +28,37 @@ function fractal( x, y, maxIterations, maxRadius ) {
 }
 
 function drawFrame( timestamp, delta ) {
-	let y = -1;
+	const MAX_ITERATIONS = 15;
+	const SCALE          = 3;
+	const SCALE_DIV2     = SCALE / 2;
+	const SCALE_DIV4     = SCALE / 4;
+	const CENTER_X       = WIDTH / 2;
+	const CENTER_Y       = HEIGHT / 2;
+	const ROT_IN_TIME    = 6 * 1000;
 
-	for( let mapIndex = 0, x = 0; mapIndex < width * height * 4; mapIndex += 4, ++x ) {
-		if( x === width ) {
+	let y = -1;
+	for( let mapIndex = 0, x = 0; mapIndex < WIDTH * HEIGHT * 4; mapIndex += 4, ++x ) {
+		if( x === WIDTH ) {
 			x = 0;
 		}
 		if( x === 0 ) {
 			++y;
 		}
 
-		const real      = x / width;
-		const imaginary = y / height;
+		const inTime     = Math.min( 0, ( timestamp - ROT_IN_TIME ) / ROT_IN_TIME );
+		const inTimeEase = ( ( inTime - 1 ) * inTime * inTime ) * ROT_IN_TIME;
+		const sin        = Math.sin( inTimeEase / 10000 );
+		const cos        = Math.cos( inTimeEase / 10000 );
 
-		const r = fractal( ( real * 2 ) - 1.5, ( imaginary * 2 ) - 1, 255, 50 );
+		const real      = ( ( x - CENTER_X ) * cos - ( y - CENTER_Y ) * sin ) / WIDTH;
+		const imaginary = ( ( x - CENTER_X ) * sin + ( y - CENTER_Y ) * cos ) / HEIGHT;
+
+		const r = fractal(
+				( real + 0.5 ) * SCALE - SCALE_DIV2 - SCALE_DIV4,
+				( imaginary + 0.5 ) * SCALE - SCALE_DIV2,
+				MAX_ITERATIONS,
+				Math.min( Math.log( ( timestamp + 2000 ) / 3000 ), 10 )
+			) / MAX_ITERATIONS * 255;
 
 		const componentRed   = r;
 		const componentGreen = r;
