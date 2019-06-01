06f63d648054cc0e201e293389ee1e73c20efaae
Oliver Salzburg <oliver.salzburg@gmail.com>
2019-06-01 11:35:19 +0200

    feat: Colors

--- main.js
+++ main.js
@@ -17,6 +17,8 @@ const ROT_IN_TIME_DIV2 = ROT_IN_TIME / 2;
 
 const pixMap = context.createImageData( WIDTH, HEIGHT );
 
+const PALETTE = [ 0x3a242b, 0x3b2426, 0x352325, 0x836454, 0x7d5533, 0x8b7352, 0xb1a181, 0xa4632e, 0xbb6b33, 0xb47249, 0xca7239, 0xd29057, 0xe0b87e, 0xd9b166, 0xf5eabe, 0xfcfadf, 0xd9d1b0, 0xfcfadf, 0xd1d1ca, 0xa7b1ac, 0x879a8c, 0x9186ad, 0x776a8e, 0x000022, 0x000022, 0x000022, 0x000022, 0x000022, 0x000022, 0x000022, 0x000022, 0x000022, 0x000022 ].reverse();
+
 function fractal( x, y, maxIterations, maxRadius ) {
 	let real      = x;
 	let imaginary = y;
@@ -73,15 +75,16 @@ function drawFrame( timestamp, delta ) {
 				Math.min( Math.log( ( timestamp  ) / 10000 ), WIDTH )
 			) / iterations * 255;
 
-		const color = r - Math.max( 0, ( ( ROT_IN_TIME_DIV2 - timestamp ) / ROT_IN_TIME_DIV2 * 255 ) );
+		const color = PALETTE[ Math.floor( r / 255 * PALETTE.length ) ];
+		const fade  = Math.max( 0, ( ( ROT_IN_TIME_DIV2 - timestamp ) / ROT_IN_TIME_DIV2 * 255 ) );
 
-		const componentRed   = color;
-		const componentGreen = color;
-		const componentBlue  = color;
+		const componentRed   = color >> 16 & 0xff;
+		const componentGreen = color >> 8 & 0xff;
+		const componentBlue  = color & 0xff;
 
-		pixMap.data[ mapIndex + 0 ] = componentRed;
-		pixMap.data[ mapIndex + 1 ] = componentGreen;
-		pixMap.data[ mapIndex + 2 ] = componentBlue;
+		pixMap.data[ mapIndex + 0 ] = componentRed - fade;
+		pixMap.data[ mapIndex + 1 ] = componentGreen - fade;
+		pixMap.data[ mapIndex + 2 ] = componentBlue - fade;
 		pixMap.data[ mapIndex + 3 ] = 255;
 	}
 
